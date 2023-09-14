35786338c251aa10d8f34e5f286fef5a290afcdb
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-14 23:58:22 +0330

    add podcast author model

---
+++ author/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from main.models import BaseModel
+
+# Create your models here.
+class PodcastAuthor(BaseModel):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
