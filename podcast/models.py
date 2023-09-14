8c9c1f542c5409b39dfa879e9f42bfe2c9345b7c
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 00:10:20 +0330

    add episode model

--- podcast/models.py
+++ podcast/models.py
@@ -51,3 +51,22 @@ class Podcast(BaseModel):
 
     def __str__(self):
         return f"{self.id}: {self.title}"
+
+#--EPISODE--#
+class Episode(models.Model):
+    title = models.CharField(max_length=100) #We use title for itunes_title too.
+    guid = models.CharField(max_length=50)
+    itunes_duration = models.CharField(max_length=50)
+    itunes_episode_type = models.CharField(max_length=50)
+    itunes_explicit = models.CharField(max_length=50)
+    description = models.TextField() #We use description for itunes summary and content_encoded too.
+    enclosure = models.CharField(max_length=50, null=True, blank=True)
+    link = models.URLField(null=True,blank=True)
+    pub_date = models.DateTimeField(null=True,blank=True)
+    itunes_keywords = models.TextField(null=True, blank=True)
+    itunes_player = models.CharField(max_length=50, null=True, blank=True) #We use itunes_player for fireside_playerEmbedCode too.
+    episode_podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    episode_author = models.ForeignKey(EpisodeAuthor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
