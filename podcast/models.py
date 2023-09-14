4bf2a60663aa075a998e903cb07ef183de8205b5
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 00:10:02 +0330

    add podcast model

--- podcast/models.py
+++ podcast/models.py
@@ -31,3 +31,23 @@ class Generator(BaseModel):
 
     def __str__(self):
         return f"{self.id}: {self.name}"
+
+class Podcast(BaseModel):
+    title = models.CharField(max_length=100)
+    language = models.CharField(max_length=50)
+    itunes_type = models.CharField(max_length=50)
+    copy_right = models.CharField(max_length=100)
+    explicit = models.CharField(max_length=50)
+    description = models.TextField() #We use description for itunes summary too.
+    pubDate = models.DateTimeField(null=True,blank=True)
+    last_build_date = models.DateTimeField(null=True,blank=True)
+    link = models.URLField(null=True,blank=True)
+    itunes_subtitle = models.TextField(null=True,blank=True)
+    itunes_keywords = models.TextField(null=True, blank=True)
+    category = models.ManyToManyField(Category)
+    pod_generator = models.ForeignKey(Generator,on_delete=models.CASCADE)
+    author = models.ForeignKey(PodcastAuthor,on_delete=models.SET_NULL, null=True)
+    pod_image = models.OneToOneField(Image,on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
