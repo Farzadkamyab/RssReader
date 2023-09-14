ba2f65f83f68167bbf76a132cdbd3e50d0748261
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-14 23:58:36 +0330

    add episode author model

--- author/models.py
+++ author/models.py
@@ -7,3 +7,10 @@ class PodcastAuthor(BaseModel):
 
     def __str__(self):
         return f"{self.id}: {self.name}"
+
+class EpisodeAuthor(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+    
