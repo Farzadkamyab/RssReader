05c9d183d6ce2769ee30c4c93db5a8ad91f033a2
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 00:03:14 +0330

    add author urls to urls

--- core/urls.py
+++ core/urls.py
@@ -20,3 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include("rest_framework.urls")),
+    path('author/', include("author.urls")),
